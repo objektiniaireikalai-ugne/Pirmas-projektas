7bd291195ee0efe53a844f0e27f073e4c0810455
objektiniaireikalai-ugne <segzdaviciuteugne@gmail.com>
2026-09-16 16:18:44 +0300

    Refactor student data input and output handling

--- objektinis_programavimas1.cpp
+++ objektinis_programavimas1.cpp
@@ -2,11 +2,13 @@
 #include <iomanip>
 #include <string>
 #include <vector>
+#include <numeric>
 using std::cin;
 using std::cout;
 using std::left;
 using std::right;
 using std::setw;
+using std::vector;
 
 struct studentas {
     std::string var,pav;
@@ -16,17 +18,35 @@ struct studentas {
 
 int main(){
     studentas A;
-    std::cout << "Įveskite vardą: "; cin >> A.var;
-    std::cout << "Įveskite pavardę: "; cin >> A.pav;
+    vector<studentas> grupe;
+    while(true)
+    {       
+    std::cout << "Iveskite varda: "; cin >> A.var;
+    std::cout << "Iveskite pavarde: "; cin >> A.pav;
     while (true){
         int n; char kl;
-        std::cout << "Įveskite sem. paz.: "; cin >> n; A.paz.push_back(n);
-        cout << "Ar studentas turi dar pažymių? t/n "; cin >> kl;
+        std::cout << "Iveskite sem. paz.: "; cin >> n; A.paz.push_back(n);
+        cout << "Ar studentas turi dar pazymiu? t/n "; cin >> kl;
         if (kl == 'n' || kl == 'N') break;
         }
-    std::cout << "Įveskite egzaminą: "; cin >> A.egz;
-    cout << "Studento duomenys: \n";
-    cout << left << setw(15) << A.var << left << setw(20) << A.pav;
-    for (int sk : A.paz) cout << right << setw(5) << sk;
-    cout << right << setw(10) << A.egz;
+    std::cout << "Iveskite egzamina: "; cin >> A.egz;
+    A.rez=0.4*std::accumulate(A.paz.begin(), A.paz.end(), 0.0)/A.paz.size() + 0.6*A.egz;
+    grupe.push_back(A);
+    A.paz.clear();
+    char kl;
+    cout << "Ar turite dar studentu? t/n "; cin >> kl;
+        if (kl == 'n' || kl == 'N') break;
+    }
+    
+    cout << "Studentu duomenys: \n";
+    cout << "|"<< left << setw(15) << "Vardas" << "|"<< left << setw(20) << "Pavarde";
+    cout << "|" << right << setw(10) << "Final" << "|\n";
+    int br=15+20+10+2;
+    cout<<"|";for (int i=0;i<br; i++) cout<<"-"; cout<<"|\n";
+
+    for (auto B : grupe)
+    {
+        cout << "|"<< left << setw(15) << B.var << "|" << left << setw(20) << B.pav;
+        cout << "|" << right << setw(10) << B.rez << "|\n";
+        }
     }
